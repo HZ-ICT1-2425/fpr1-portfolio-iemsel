5507d8866cc8499787a73d15aad8eacb3ea8c7b0
Naimi de Jong <jong0394@hz.nl>
2025-02-16 22:58:17 +0100

    code analysis fix

--- resources/views/components/faq/faq-item.blade.php
+++ resources/views/components/faq/faq-item.blade.php
@@ -1,4 +1,46 @@
- <details class="faq_item">
+<style>
+    body {font-family: Arial, Helvetica, sans-serif;}
+
+    /* The Modal (background) */
+    .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        padding-top: 100px; /* Location of the box */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+
+    /* Modal Content */
+    .modal-content {
+        background-color: #2C2C34;
+        margin: auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 40%;
+    }
+
+    /* The Close Button */
+    .close {
+        color: #aaaaaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+    }
+</style>
+<details class="faq_item">
         <summary class="faq_title">{{ $faq->question }}</summary>
         <div class="faq_content">
             <p>{{ $faq->answer }}</p>
